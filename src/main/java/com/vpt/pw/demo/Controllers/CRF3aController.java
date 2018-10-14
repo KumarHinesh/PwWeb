974aeaf02c9421461ac3dfc4ce6c2d589abea439
chotabacha <hknarwani7@gmail.com>
2018-10-14 14:29:58 +0500

    add controllers

---
+++ src/main/java/com/vpt/pw/demo/Controllers/CRF3aController.java
@@ -0,0 +1,28 @@
+package com.vpt.pw.demo.Controllers;
+
+
+import com.vpt.pw.demo.dtos.crf1DTO.FormCrf1DTO;
+import com.vpt.pw.demo.service.CRF1Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/form/crf3a")
+public class CRF3aController {
+
+    @Autowired
+    private CRF1Service crf1Service;
+
+    //@PostMapping(value = "/register", produces="application/json", consumes="application/json")
+    @PostMapping("/save")
+    public String registerPW(@RequestBody FormCrf1DTO form) {
+        String resp = crf1Service.registerPw(form);
+
+        return resp;
+    }
+
+}
